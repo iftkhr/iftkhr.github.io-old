7dedda1f7f57c1a0a2c664c97ec050bfbdb17708
Iftakhar Kaunain Ashhar <iftakhar.ashhar@gmail.com>
2020-07-23 10:46:34 +0530

    navigation

---
+++ js/portfolio.js
@@ -0,0 +1,15 @@
+// top navigation bar on mobile
+function topNavOpen(){
+document.getElementById("top").classList.remove("nav-hide");
+document.getElementById("dots").classList.add("nav-hide");
+document.getElementById("cross").classList.remove("nav-hide");
+}
+function topNavClose(){
+    document.getElementById("top").classList.add("nav-hide");
+    document.getElementById("dots").classList.remove("nav-hide");
+    document.getElementById("cross").classList.add("nav-hide");
+}
+
+// carousel for projects and certificates on mobile
+
+
